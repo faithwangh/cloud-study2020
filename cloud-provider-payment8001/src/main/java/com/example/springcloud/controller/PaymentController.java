5f14be93b7c61f0388e8b75bad56a86dfdbad6db
faith <704080119@qq.com>
2020-07-07 17:55:45 +0800

    Add payment controller service

---
+++ cloud-provider-payment8001/src/main/java/com/example/springcloud/controller/PaymentController.java
@@ -0,0 +1,47 @@
+package com.example.springcloud.controller;
+
+import com.example.springcloud.entities.CommentResult;
+import com.example.springcloud.entities.Payment;
+import com.example.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Objects;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping("/payment")
+    public CommentResult create(Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("****插入结果：" + result);
+
+        if (result > 0) {
+            return new CommentResult(200, "插入成功", null);
+        } else {
+            return new CommentResult(500, "插入失败", null);
+        }
+    }
+
+    @GetMapping("/payment/{id}")
+    public CommentResult getPaymentById(@PathVariable("id") Long id){
+
+        Payment payment = paymentService.getPaymentById(id);
+
+        if (payment != null) {
+            return new CommentResult(200, "查询成功", payment);
+        } else {
+            return new CommentResult(500, "没有对应的记录", null);
+        }
+    }
+
+
+}
